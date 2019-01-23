0606959cd39ec9ed35cb2e74814f2685aa23c2b5
wuhx <546446588@qq.com>
2019-01-23 10:24:34 +0800

    Add number

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Welcome to Flutter',
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Welcome to Flutter'),
-          ),
-          body: new Center(
-            child: Image(
-              image: AssetImage("images/long.jpg"),
-            ),
-          ),
-        ));
+        title: 'Fltter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage(title: 'Flutter Demo Home Page'));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  @override
+  State<StatefulWidget> createState() {
+    print('createState()');
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ///数量
+  int _counter = 0;
+
+  void _incrementCounter() {
+    print('点击');
+
+    ///通知框架这个对象的内部状态已经改变。
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        ///column 垂直数组中显示其子元素的小部件。
+        child: Column(
+          ///沿主轴放置子元素
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          ///元素集合
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        ///点击后调用的方法
+        onPressed: _incrementCounter,
+
+        ///点击后的提示语
+        tooltip: 'Increment',
+
+        ///图标
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
